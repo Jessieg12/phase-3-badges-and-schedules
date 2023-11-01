a144b91f990e5549d731fcd91bfdbcdcc1387881
Jessie Guzman <jessieguzman777@gmail.com>
2023-11-01 11:26:35 -0500

    commit tree 834a945c80e64945ebf7c4559886425bcb142137

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,38 @@
 # Write your code here.
+require "pry"
+
+
+def badge_maker(speaker)
+  return "Hello, my name is #{speaker}."
+end
+
+def batch_badge_creator(array)
+  spit = array.map do |name|
+    "Hello, my name is #{name}."
+  end
+  return spit
+end
+
+def assign_rooms(speakers)
+  rooms = []
+  speakers.each_with_index do |speaker, index|
+    room = index +1
+    agent = "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    rooms << agent
+  end
+  return rooms
+end
+
+def printer(attendees)
+  badge = batch_badge_creator(attendees)
+  rooms = assign_rooms(attendees)
+
+  badge.each do |person|
+    puts person
+  end
+  rooms.each do |person|
+    puts person
+  end
+end
+
+binding.pry
